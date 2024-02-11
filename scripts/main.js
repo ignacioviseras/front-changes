c6202005d7024652127325fe942222299983d6cc
ignacioviseras <ignacio.viseras@gmail.com>
2024-02-11 12:40:54 +0100

    add innersHtml

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-import * as loginModule from "../login/login.js";
-
-loginModule.callback42();
-loginModule.displayLoginForm();
-loginModule.login();
-loginModule.register();
-loginModule.importLogin();
-loginModule.inicializarEventos();
